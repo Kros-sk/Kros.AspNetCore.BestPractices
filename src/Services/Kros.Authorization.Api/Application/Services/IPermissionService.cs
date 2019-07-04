87b216af91730d6dc1ef363c5ede9bb6d4ad97a9
kubinko <jakub.berthoty@gmail.com>
2019-07-04 12:42:03 +0200

    Permission service.

---
+++ src/Services/Kros.Authorization.Api/Application/Services/IPermissionService.cs
@@ -0,0 +1,36 @@
+﻿using Kros.Authorization.Api.Application.Commands.UpdatePermissions;
+using Kros.Authorization.Api.Application.Model;
+using Kros.Authorization.Api.Application.Queries.Permission;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Kros.Authorization.Api.Application.Services
+{
+    /// <summary>
+    /// Interface which describe service for working with <see cref="Permission"/>.
+    /// </summary>
+    public interface IPermissionService
+    {
+        /// <summary>
+        /// Gets all user permissions.
+        /// </summary>
+        /// <param name="userClaims">All user claims (contains user id and organization id).</param>
+        /// <returns>User permissions.</returns>
+        Task<IEnumerable<GetAllUserPermissionsQuery.Permission>> GetAllUserPermissionsAsync(IEnumerable<Claim> userClaims);
+
+        /// <summary>
+        /// Gets user permissions related to current organization.
+        /// </summary>
+        /// <param name="userClaims">All user claims (contains user id and organization id).</param>
+        /// <returns>User permissions.</returns>
+        Task<IEnumerable<GetUserPermissionsForOrganizationQuery.Permission>> GetUserPermissionsByOrganizationAsync(
+            IEnumerable<Claim> userClaims);
+
+        /// <summary>
+        /// Updates user permissions.
+        /// </summary>
+        /// <param name="command">Update command.</param>
+        Task UpdateUserPermissionsAsync(UpdatePermissionsCommand command);
+    }
+}
