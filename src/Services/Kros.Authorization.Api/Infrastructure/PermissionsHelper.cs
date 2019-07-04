b6cbd53dd1d9f62e173dc8c16e3b024d98fd845e
kubinko <jakub.berthoty@gmail.com>
2019-07-04 12:42:03 +0200

    Permission service.

---
+++ src/Services/Kros.Authorization.Api/Infrastructure/PermissionsHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace Kros.Authorization.Api.Infrastructure
+{
+    public static class PermissionsHelper
+    {
+        public static class Headers
+        {
+            /// <summary>
+            /// Name of HTTP request header containing organization ID.
+            /// </summary>
+            public const string OrganizationIdHeader = "Kros_OrganizationId";
+        }
+
+        public static class Claims
+        {
+            /// <summary>
+            /// Organization ID.
+            /// </summary>
+            public const string OrganizationId = "organization_id";
+        }
+    }
+}
