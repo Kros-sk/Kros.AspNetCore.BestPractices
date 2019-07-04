485aff6c35b24a0164956185553b140668ed8d92
kubinko <jakub.berthoty@gmail.com>
2019-07-04 11:55:16 +0200

    CQRS for permissions.

---
+++ src/Services/Kros.Authorization.Api/Application/Queries/Permission/GetUserPermissionsQueryHandler.cs
@@ -0,0 +1,45 @@
+﻿using Kros.KORM;
+using Kros.Utils;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kros.Authorization.Api.Application.Queries.Permission
+{
+    /// <summary>
+    /// Query handler for user permissions queries.
+    /// </summary>
+    public class GetUserPermissionsQueryHandler :
+        IRequestHandler<GetUserPermissionsForOrganizationQuery, IEnumerable<GetUserPermissionsForOrganizationQuery.Permission>>,
+        IRequestHandler<GetAllUserPermissionsQuery, IEnumerable<GetAllUserPermissionsQuery.Permission>>
+    {
+        private readonly IDatabase _database;
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        /// <param name="database">Database.</param>
+        public GetUserPermissionsQueryHandler(IDatabase database)
+        {
+            _database = Check.NotNull(database, nameof(database));
+        }
+
+        /// <inheritdoc />
+        public Task<IEnumerable<GetUserPermissionsForOrganizationQuery.Permission>> Handle(
+            GetUserPermissionsForOrganizationQuery request,
+            CancellationToken cancellationToken)
+            => Task.FromResult(_database.Query<GetUserPermissionsForOrganizationQuery.Permission>()
+                                        .Where(p => p.UserId == request.UserId && p.OrganizationId == request.OrganizationId)
+                                        .AsEnumerable());
+
+        /// <inheritdoc />
+        public Task<IEnumerable<GetAllUserPermissionsQuery.Permission>> Handle(
+            GetAllUserPermissionsQuery request,
+            CancellationToken cancellationToken)
+            => Task.FromResult(_database.Query<GetAllUserPermissionsQuery.Permission>()
+                                        .Where(p => p.UserId == request.UserId)
+                                        .AsEnumerable());
+    }
+}
