6a33bfe2e0173406e05e68da360499602b01f5ef
kubinko <jakub.berthoty@gmail.com>
2019-07-08 07:38:04 +0200

    Custom authorization.

---
+++ src/ApiGateway/Authorization/GatewayJwtAuthorizationOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace ApiGateway.Authorization
+{
+    /// <summary>
+    /// JWT authorization options for api gateway.
+    /// </summary>
+    public class GatewayJwtAuthorizationOptions
+    {
+        public GatewayJwtAuthorizationOptions() { }
+
+        /// <summary>
+        /// Authorization service url.
+        /// </summary>
+        public string AuthorizationUrl { get; set; }
+
+        /// <summary>
+        /// Set of headers to preserve from original request.
+        /// </summary>
+        public string[] HeadersToPreserve { get; set; }
+    }
+}
