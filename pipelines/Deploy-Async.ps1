842873f2eb219a1b27660c78fa2dc97595c431c0
Miňo Martiniak <mino.martiniak@gmail.com>
2020-02-18 13:21:35 +0100

    Update Deploy-Async.ps1

--- pipelines/Deploy-Async.ps1
+++ pipelines/Deploy-Async.ps1
@@ -1,12 +1,16 @@
 param (
-    [Parameter(Mandatory = $true)][String[]]$microservices
+    [Parameter(Mandatory = $true)][String[]]$microservices,
+    [Parameter(Mandatory = $true)][String]$resourceGroup,
+    [Parameter(Mandatory = $true)][String]$appNameTemplate,
+    [Parameter(Mandatory = $true)][String]$servicePathTemplate
 )
 
 $jobs = @()
 ForEach ($service in $microservices) {
     $jobs += Start-Job -ArgumentList $service -ScriptBlock {
         param($name)
-            az webapp deployment source config-zip --resource-group kros-dev-meetups-rsg --name demo-dev-$name-api --src "$(Pipeline.Workspace)/ToDosDemoServices/drop/Kros.$name.Api.zip"
+            # az webapp deployment source config-zip --resource-group kros-dev-meetups-rsg --name demo-dev-$name-api --src "$(Pipeline.Workspace)/ToDosDemoServices/drop/Kros.$name.Api.zip"
+            Write-Host "Spustam nasadenie " + $name
     }
 }
 
