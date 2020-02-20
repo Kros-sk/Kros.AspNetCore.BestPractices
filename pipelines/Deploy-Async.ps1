028fd09da10ce3f8e10ae70e6319c870444ff608
Burgyn <mino.martiniak@gmail.com>
2020-02-20 11:46:29 +0100

    aaa

--- pipelines/Deploy-Async.ps1
+++ pipelines/Deploy-Async.ps1
@@ -1,5 +1,6 @@
 param (
-    [Parameter(Mandatory = $true)][String[]]$microservices
+    [Parameter(Mandatory = $true)][String[]]$microservices,
+    [Parameter(Mandatory = $true)][String]$artifactPath
 )
 
 $jobs = @()
@@ -7,7 +8,7 @@ ForEach ($service in $microservices) {
     $jobs += Start-Job -ArgumentList $service -ScriptBlock {
         param($name)
             Write-Host "Deploying microservice: " $name
-            az webapp deployment source config-zip --resource-group kros-demo-rsg --name kros-demo-$name-api --src $(Pipeline.Workspace)/ToDosDemoServices/drop/Kros.$name.Api.zip
+            az webapp deployment source config-zip --resource-group kros-demo-rsg --name kros-demo-$name-api --src "$artifactPath/Kros.$name.Api.zip"
     }
 }
 
