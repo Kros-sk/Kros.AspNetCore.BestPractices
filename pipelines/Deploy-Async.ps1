afd0bad5d3f1b6c84b351cd2b2dfd5352104169e
Burgyn <mino.martiniak@gmail.com>
2020-02-20 12:08:12 +0100

    aaa

--- pipelines/Deploy-Async.ps1
+++ pipelines/Deploy-Async.ps1
@@ -3,30 +3,31 @@ param (
     [Parameter(Mandatory = $true)][String]$artifactPath
 )
 
-$jobs = @()
+# $jobs = @()
 ForEach ($service in $microservices) {
-    $jobs += Start-Job -ArgumentList $service -ScriptBlock {
-        param($name)
-            Write-Host "Deploying microservice: " $name
-            az webapp deployment source config-zip --resource-group kros-demo-rsg --name kros-demo-$name-api --src "$artifactPath/Kros.$name.Api.zip"
-            Write-Host "Deployed microservice: " $name
-    }
+    # $jobs += Start-Job -ArgumentList $service -ScriptBlock {
+    #     param($name)
+    #         Write-Host "Deploying microservice: " $name
+    #         az webapp deployment source config-zip --resource-group kros-demo-rsg --name kros-demo-$name-api --src "$artifactPath/Kros.$name.Api.zip"
+    #         Write-Host "Deployed microservice: " $name
+    # }
+    az webapp deployment source config-zip --resource-group kros-demo-rsg --name kros-demo-$service-api --src "$artifactPath/Kros.$name.Api.zip"
 }
 
-Wait-Job -Job $jobs | Out-Null
+# Wait-Job -Job $jobs | Out-Null
 
-$failed = $false
+# $failed = $false
 
-foreach ($job in $jobs) {
-    if ($job.State -eq 'Failed') {
-        $failed = $true
-        Write-Host ($job.ChildJobs[0].JobStateInfo.Reason.Message) -ForegroundColor Red
-    } else {
-        Write-Host (Receive-Job $job) -ForegroundColor Green
-    }
-}
+# foreach ($job in $jobs) {
+#     if ($job.State -eq 'Failed') {
+#         $failed = $true
+#         Write-Host ($job.ChildJobs[0].JobStateInfo.Reason.Message) -ForegroundColor Red
+#     } else {
+#         Write-Host (Receive-Job $job) -ForegroundColor Green
+#     }
+# }
 
-if ($failed -eq $true)
-{
-    Write-Error "Microservices deploy failed."
-}
+# if ($failed -eq $true)
+# {
+#     Write-Error "Microservices deploy failed."
+# }
