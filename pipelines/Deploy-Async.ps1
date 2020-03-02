f11c7616080e0c4eabe341d4a464e6dae5eacf5b
Miňo Martiniak <mino.martiniak@gmail.com>
2020-03-02 10:28:13 +0100

    Update Deploy-Async.ps1

--- pipelines/Deploy-Async.ps1
+++ pipelines/Deploy-Async.ps1
@@ -7,6 +7,7 @@ $jobs = @()
 ForEach ($service in $microservices) {
    # $jobs += Start-Job -ArgumentList $service -ScriptBlock {
    #     param($name)
+            $name = $service
             Write-Host "Deploying microservice: " $name
             az webapp deployment source config-zip --resource-group kros-demo-rsg --name kros-demo-$name-api --src "$artifactPath/Kros.$name.Api.zip"
             Write-Host "Deployed microservice: " $name
