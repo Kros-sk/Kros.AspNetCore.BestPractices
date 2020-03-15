758de7a7e88b64fb0e91dd6d36ba29f2451f1e3d
Miňo Martiniak <mino.martiniak@gmail.com>
2020-03-15 14:37:28 +0100

    Support for async deploy

--- pipelines/Deploy-Async.ps1
+++ pipelines/Deploy-Async.ps1
@@ -5,27 +5,28 @@ param (
 
 $jobs = @()
 ForEach ($service in $microservices) {
-   # $jobs += Start-Job -ArgumentList $service -ScriptBlock {
-   #     param($name)
-            $name = $service
-            Write-Host "Deploying microservice: " $name
-            az webapp deployment source config-zip --resource-group kros-demo-rsg --name kros-demo-$name-api --src "$artifactPath/Kros.$name.Api.zip"
-            Write-Host "Deployed microservice: " $name
-   # }
+    Write-Host "Start deploying microservice: " $service -ForegroundColor Green
+    $jobs += Start-Job -ArgumentList $service -ScriptBlock {
+        param($name)
+            $result = az webapp deployment source config-zip --resource-group kros-demo-rsg --name kros-demo-$name-api --src "$artifactPath/Kros.$name.Api.zip" 
+            if ($result){
+                Write-Host "Deployed microservice: " $name -ForegroundColor Green
+            }
+    }
 }
 
-#Wait-Job -Job $jobs | Out-Null
+Wait-Job -Job $jobs
 
 $failed = $false
 
-#foreach ($job in $jobs) {
-#    if ($job.State -eq 'Failed') {
-#        $failed = $true
-#        Write-Host ($job.ChildJobs[0].JobStateInfo.Reason.Message) -ForegroundColor Red
-#    }
-#}
+foreach ($job in $jobs) {
+    Receive-Job $job -Keep
+    if ($job.ChildJobs[0].Error -ne '') {
+        $failed = $true
+    }
+}
 
-#if ($failed -eq $true)
-#{
-#    Write-Error "Microservices deploy failed."
-#}
+if ($failed -eq $true) {
+    Write-Host 
+    Write-Error "Microservices deploy failed."
+}
