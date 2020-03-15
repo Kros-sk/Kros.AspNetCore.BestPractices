478330e466f2cd1567bd747e850adfc685d7f732
Miňo Martiniak <mino.martiniak@gmail.com>
2020-03-15 14:49:31 +0100

    Update Deploy-Async.ps1

--- pipelines/Deploy-Async.ps1
+++ pipelines/Deploy-Async.ps1
@@ -3,31 +3,31 @@ param (
     [Parameter(Mandatory = $true)][String]$artifactPath
 )
 
-#$jobs = @()
+Enable-AzContextAutosave
+$jobs = @()
 ForEach ($service in $microservices) {
     Write-Host "Start deploying microservice: " $service -ForegroundColor Green
-   # $jobs += Start-Job -ArgumentList $service -ScriptBlock {
-   #     param($name)
-   $name = $service
+    $jobs += Start-Job -ArgumentList $service -ScriptBlock {
+        param($name)
             $result = az webapp deployment source config-zip --resource-group kros-demo-rsg --name kros-demo-$name-api --src "$artifactPath/Kros.$name.Api.zip" 
             if ($result){
                 Write-Host "Deployed microservice: " $name -ForegroundColor Green
             }
-   # }
+    }
 }
 
-#Wait-Job -Job $jobs
+Wait-Job -Job $jobs
 
-#$failed = $false
+$failed = $false
 
-#foreach ($job in $jobs) {
-#    Receive-Job $job -Keep
-#    if ($job.ChildJobs[0].Error -ne '') {
-#        $failed = $true
-#    }
-#}
+foreach ($job in $jobs) {
+    Receive-Job $job -Keep
+    if ($job.ChildJobs[0].Error -ne '') {
+        $failed = $true
+    }
+}
 
-#if ($failed -eq $true) {
-#    Write-Host 
-#    Write-Error "Microservices deploy failed."
-#}
+if ($failed -eq $true) {
+    Write-Host 
+    Write-Error "Microservices deploy failed."
+}
