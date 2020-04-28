3970ff01d87eefad6352a5e2c55d605011978e13
Majo Mikula <majco333@gmail.com>
2020-04-28 07:52:42 +0200

    Add comments

--- src/Services/Kros.Organizations.Api/Application/Services/IUserRoleService.cs
+++ src/Services/Kros.Organizations.Api/Application/Services/IUserRoleService.cs
@@ -2,9 +2,22 @@
 
 namespace Kros.Organizations.Api.Application.Services
 {
-    public interface IUserRoleService
+	/// <summary>
+	/// Interface for user role service.
+	/// </summary>
+	public interface IUserRoleService
     {
+		/// <summary>
+		/// Create owner role for user in organization.
+		/// </summary>
+		/// <param name="userId">User id.</param>
+		/// <param name="organizationId">Organization id.</param>
         Task CreateOwnerRoleAsync(long userId, long organizationId);
-        Task DeleteUserRolesAsync(long organizationId);
+
+		/// <summary>
+		/// Delete user role in organization.
+		/// </summary>
+		/// <param name="organizationId">Organization id.</param>
+		Task DeleteUserRolesAsync(long organizationId);
     }
 }
