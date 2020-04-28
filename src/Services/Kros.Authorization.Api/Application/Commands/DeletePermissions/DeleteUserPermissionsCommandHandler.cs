3201a4158e01003d6142f0b23bbe7ba431a1344d
Majo Mikula <majco333@gmail.com>
2020-04-28 07:26:31 +0200

    Fix bug with handling deleting commands for permissions

--- src/Services/Kros.Authorization.Api/Application/Commands/DeletePermissions/DeleteUserPermissionsCommandHandler.cs
+++ src/Services/Kros.Authorization.Api/Application/Commands/DeletePermissions/DeleteUserPermissionsCommandHandler.cs
@@ -6,31 +6,41 @@ using System.Threading.Tasks;
 
 namespace Kros.Authorization.Api.Application.Commands.DeletePermissions
 {
-    /// <summary>
-    /// Delete user permissions command handler.
-    /// </summary>
-    public class DeleteUserPermissionsCommandHandler
-    {
-        /// <summary>
-        /// MediatR command handler for command to delete user role.
-        /// </summary>
-        private readonly IPermissionRepository _repository;
+	/// <summary>
+	/// Delete user permissions command handler.
+	/// </summary>
+	public class DeleteUserPermissionsCommandHandler :
+		IRequestHandler<DeleteAllPermissionsByOrganizationCommand, Unit>,
+		IRequestHandler<DeleteUserPermissionsByOrganizationCommand, Unit>
+	{
+		/// <summary>
+		/// MediatR command handler for command to delete user role.
+		/// </summary>
+		private readonly IPermissionRepository _repository;
 
-        /// <summary>
-        /// Ctor.
-        /// </summary>
-        /// <param name="repository">User role repository.</param>
-        public DeleteUserPermissionsCommandHandler(IPermissionRepository repository)
-        {
-            _repository = Check.NotNull(repository, nameof(repository));
-        }
+		/// <summary>
+		/// Ctor.
+		/// </summary>
+		/// <param name="repository">User role repository.</param>
+		public DeleteUserPermissionsCommandHandler(IPermissionRepository repository)
+		{
+			_repository = Check.NotNull(repository, nameof(repository));
+		}
 
-        /// <inheritdoc />
-        public async Task<Unit> Handle(DeleteUserPermissionsByOrganizationCommand request, CancellationToken cancellationToken)
-        {
-            await _repository.DeleteUserRolesInOrganizationAsync(request.OrganizationId, request.UserId);
+		/// <inheritdoc />
+		public async Task<Unit> Handle(DeleteUserPermissionsByOrganizationCommand request, CancellationToken cancellationToken)
+		{
+			await _repository.DeleteUserRolesInOrganizationAsync(request.OrganizationId, request.UserId);
 
-            return Unit.Value;
-        }
-    }
+			return Unit.Value;
+		}
+
+		/// <inheritdoc />
+		public async Task<Unit> Handle(DeleteAllPermissionsByOrganizationCommand request, CancellationToken cancellationToken)
+		{
+			await _repository.DeleteAllUserRolesInOrganizationAsync(request.OrganizationId);
+
+			return Unit.Value;
+		}
+	}
 }
