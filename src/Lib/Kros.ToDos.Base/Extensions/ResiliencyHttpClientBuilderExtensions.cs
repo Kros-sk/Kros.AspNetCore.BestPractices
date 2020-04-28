851bd0461ba7039b4c77dcf23253b5f96c424654
Majo Mikula <majco333@gmail.com>
2020-04-28 06:59:18 +0200

    After RF (remove oddly intending)

--- src/Lib/Kros.ToDos.Base/Extensions/ResiliencyHttpClientBuilderExtensions.cs
+++ src/Lib/Kros.ToDos.Base/Extensions/ResiliencyHttpClientBuilderExtensions.cs
@@ -7,61 +7,62 @@ using System.Net.Http;
 
 namespace Kros.ToDos.Base.Extensions
 {
-    /// <summary>
-    /// Extensions methods for configuring <see cref="PolicyHttpMessageHandler"/> message handlers as part of
-    /// and <see cref="HttpClient"/> message handler pipeline.
-    /// </summary>
-    public static class ResiliencyHttpClientBuilderExtensions
-    {
-        private const int DefaultRetryCount = 5;
-        private const int DefaultFirstExponentialWaitTimeInMilliseconds = 500;
+	/// <summary>
+	/// Extensions methods for configuring <see cref="PolicyHttpMessageHandler"/> message handlers as part of
+	/// and <see cref="HttpClient"/> message handler pipeline.
+	/// </summary>
+	public static class ResiliencyHttpClientBuilderExtensions
+	{
+		private const int DefaultRetryCount = 5;
+		private const int DefaultFirstExponentialWaitTimeInMilliseconds = 500;
 
-        private const int DefaultHandledEventsAllowedBeforeBreakingCount = 3;
-        private const int DefaultDurationOfBreakInSeconds = 15;
-        
-        /// <summary>
-        /// Adds default's <see cref="PolicyHttpMessageHandler"/> handlers for resiliency.
-        /// </summary>
-        /// <param name="builder">The <see cref="IHttpClientBuilder"/>.</param>
-        /// <returns>An <see cref="IHttpClientBuilder"/> that can be used to configure the client.</returns>
-        /// <remarks>
-        /// <para>
-        /// See the remarks on <see cref="PolicyHttpMessageHandler"/> for guidance on configuring policies.
-        /// </para>
-        /// </remarks>
-        public static IHttpClientBuilder AddResiliencyDefaultPolicyHandler(this IHttpClientBuilder builder)
-        {
-            if (builder == null)
-            {
-                throw new ArgumentNullException(nameof(builder));
-            }
+		private const int DefaultHandledEventsAllowedBeforeBreakingCount = 3;
+		private const int DefaultDurationOfBreakInSeconds = 15;
 
-            IAsyncPolicy<HttpResponseMessage> retryPolicy = GetRetryPolicy();
+		/// <summary>
+		/// Adds default's <see cref="PolicyHttpMessageHandler"/> handlers for resiliency.
+		/// </summary>
+		/// <param name="builder">The <see cref="IHttpClientBuilder"/>.</param>
+		/// <returns>An <see cref="IHttpClientBuilder"/> that can be used to configure the client.</returns>
+		/// <remarks>
+		/// <para>
+		/// See the remarks on <see cref="PolicyHttpMessageHandler"/> for guidance on configuring policies.
+		/// </para>
+		/// </remarks>
+		public static IHttpClientBuilder AddResiliencyDefaultPolicyHandler(this IHttpClientBuilder builder)
+		{
+			if (builder is null)
+			{
+				throw new ArgumentNullException(nameof(builder));
+			}
+
+			IAsyncPolicy<HttpResponseMessage> retryPolicy = GetRetryPolicy();
 			IAsyncPolicy<HttpResponseMessage> circuitBreakerPolicy = GetCircuitBreakerPolicy();
 
 			builder.AddHttpMessageHandler(() => new PolicyHttpMessageHandler(retryPolicy.WrapAsync(circuitBreakerPolicy)));
 
-            return builder;
-        }
+			return builder;
+		}
 
-        private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
-        {
-            Random jitterer = new Random();
-            return HttpPolicyExtensions
-                .HandleTransientHttpError()
+		private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+		{
+			Random jitterer = new Random();
+			return HttpPolicyExtensions
+				.HandleTransientHttpError()
 				.WaitAndRetryAsync(DefaultRetryCount,
-                    retryAttempt => {
+					retryAttempt =>
+					{
 						return TimeSpan.FromMilliseconds(DefaultFirstExponentialWaitTimeInMilliseconds * Math.Pow(2, retryAttempt))
 							+ TimeSpan.FromMilliseconds(jitterer.Next(0, 100));
 					}
-                );
-        }
+				);
+		}
 
-        private static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
-        {
-            return HttpPolicyExtensions
-                .HandleTransientHttpError()
+		private static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+		{
+			return HttpPolicyExtensions
+				.HandleTransientHttpError()
 				.CircuitBreakerAsync(DefaultHandledEventsAllowedBeforeBreakingCount, TimeSpan.FromSeconds(DefaultDurationOfBreakInSeconds));
-        }
-    }
+		}
+	}
 }
